816cd7b3cb2929b7926f9bfe7ea4cbcfb8efab6b
sslogar <sslogar@ufl.edu>
2019-10-14 18:18:35 -0400

    start of topic clustering

--- bushTopics.py
+++ bushTopics.py
@@ -3,9 +3,10 @@ import nltk
 import re
 import string
 from nltk.tokenize import sent_tokenize, word_tokenize
-from sentiment_module import sentiment
-import seaborn as sns
-import matplotlib.pyplot as plt
+import numpy as np
+import sklearn
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.feature_extraction.text import CountVectorizer
 
 stop_words = nltk.corpus.stopwords.words('english')
 
@@ -62,51 +63,17 @@ def clean(term_vec):
         term_vec[i] = term_list
     return term_vec
 
-def return_sentiment(term_vec):
-    sent_v = []
-    for term in term_vec:
-        s = sentiment.sentiment(term)
-        sent_v.append(s) #append each dictionary returned by the sentiment function into a list
-    return sent_v
+def rebuild(term_vec):
+    for i in range(0, len(term_vec)):
+        doc = ""
+        doc = ' '.join(term_vec[i])
+        term_vec[i] = doc
+    return term_vec
 
 term_vec2001 = tokenize_words(sent2001)
 term_vec2001 = clean(term_vec2001)
 term_vec2001 = remove_stop_words(term_vec2001, stop_words)
 term_vec2001 = porter_stem(term_vec2001)
-sentiment2001 = return_sentiment(term_vec2001)
-s_2001 = pd.DataFrame(sentiment2001) #make a DataFrame out of the dictionary returned from the sentiment function
-
-def show_graphs(df):
-    sns.set()
-    # Plot scatterplot of arousal vs valence
-    ax = sns.scatterplot(x="arousal", y="valence", data=df)
-    plt.show()
-
-    ax = sns.distplot(df['arousal']) #distribution of arousal
-    plt.show()
-
-    ax = sns.distplot(df['valence']) #distribution of valence
-    plt.show()
-
-term_vec2005 = tokenize_words(sent2005)
-term_vec2005 = clean(term_vec2005)
-term_vec2005 = remove_stop_words(term_vec2005, stop_words)
-term_vec2005 = porter_stem(term_vec2005)
-
-sentiment2005 = return_sentiment(term_vec2005)
-s_2005 = pd.DataFrame(sentiment2005)
-
-# show_graphs(s_2005)
-# there appears to be a difference in sentiment between 2001 and 2005
-
-s_2005['year'] = '2005'
-s_2001['year'] = '2001'
-overall_sentiment = pd.concat([s_2001, s_2005])
-
-ax = sns.scatterplot(x="arousal", y="valence", style='year', data=overall_sentiment, s=75)
-plt.show()
-
-## QUESTION:
-#u00 removal
-#LDA topic clustering, LDA matrix first and then MST?
-# T tests, anova with sentiment?
+docs = rebuild(term_vec2001)
+for d in docs:
+    print(d)
